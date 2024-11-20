07ecadb4ba987d6efdda6039f18d7f030a663037
ayu <ayuabc4528@gmail.com>
2024-11-20 10:59:24 +0900

    Display data using axios

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">hello world</div>
-  );
+    const [character, setCharacters] = useState([]);
+
+    useEffect(() => {
+        fetchCharacters();
+    }, []);
+
+    const fetchCharacters = async () => {
+        const apiUrl = "https://narutodb.xyz/api/character";
+
+        const result = await axios.get(apiUrl);
+        setCharacters(result.data.characters);
+    };
+
+    return (
+        <div className="container">
+            <main>
+                <div className="cards-container">
+                    {character.map((character) => {
+                        return (
+                            <div className="card" key={character.id}>
+                                <img src={character.images[0] != null ? character.images[0] : "dummy.png"} alt="character" className="card-image" />
+                                <div className="card-content">
+                                    <h3 className="card-title">{character.name}</h3>
+                                    <p className="card-description">{character.debut?.appearsIn ?? "なし"}</p>
+                                    <div className="card-footer">
+                                        <span className="affiliation">{character.personal?.affiliation ?? "なし"}</span>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
